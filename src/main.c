517a3e86de95fda90ff2db138bcd5890d3d01e16
amirali <me.amiralisalimi@gmail.com>
2024-01-24 15:05:40 -0500

    Add graceful exit

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <regex.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -71,6 +72,8 @@ char *redirect_from_dest(const Route *route, const char *http_req) {
         perror("unable to read from destination");
         return NULL;
     }
+
+    close(dest_fd);
     return buffer;
 }
 
@@ -93,32 +96,50 @@ char *get_response(int client_fd, const char *buffer) {
             // TODO: send file to http request
         }
     }
+    return NULL;
 }
 
 void *handle_req(void *arg) {
     int client_fd = *((int*) arg);
     char *buffer = (char*) malloc(BUFFER_SIZE * sizeof(char));
+    char *response = NULL;
 
     ssize_t bytes_recvd = recv(client_fd, buffer, BUFFER_SIZE, 0);
     if (bytes_recvd > 0) {
-        char *response = get_response(client_fd, buffer);
-        if (response == NULL) {
-            response = HTTP_RESPONSE_BAD_REQUEST;
-        }
-        send(client_fd, response, sizeof(response), 0);
-        free(response);
+        response = get_response(client_fd, buffer);
+        if (response)
+            send(client_fd, response, sizeof(response), 0);
+        else
+            send(client_fd, HTTP_RESPONSE_BAD_REQUEST, sizeof(HTTP_RESPONSE_BAD_REQUEST), 0);
     }
     close(client_fd);
+    if (response)
+        free(response);
     free(arg);
     free(buffer);
     return NULL;
 }
 
+int server_fd;
+pthread_t threads[MAX_THREADS] = {0};
+int cur_thread = 0;
+
+void graceful_exit(int signum) {
+    printf("graceful exit\n");
+    for (int i = 0; i < MAX_THREADS; i++) {
+        if (threads[i])
+            pthread_cancel(threads[i]);
+    }
+    close(server_fd);
+    exit(0);
+}
+
 int main(int argc, char *argv[]) {
+    signal(SIGINT, graceful_exit);
     read_config_file();
     print_all_config();  // Just for test
     int port = (argc > 1 ? atoi(argv[1]) : 8000);
-    int server_fd = initsock(port);
+    server_fd = initsock(port);
 
     for (;;) {
         struct sockaddr_in client_addr;
@@ -135,6 +156,8 @@ int main(int argc, char *argv[]) {
         pthread_t thread_id;
         pthread_create(&thread_id, NULL, handle_req, (void*) client_fd);
         pthread_detach(thread_id);
+        threads[cur_thread++] = thread_id;
+        cur_thread %= MAX_THREADS;
     }
 
     close(server_fd);
